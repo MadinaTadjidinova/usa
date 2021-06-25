5723094b801dee293faec4829f667873bf2395dc
MadinaTadjidinova <madina.tadjidinova@gmail.com>
2021-06-25 10:14:00 +0600

    js file

---
+++ js/rtyuhio.js
@@ -0,0 +1,795 @@
+
+// @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@800&display=swap'); 
+// @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@600&display=swap');
+// .col .h1{
+//  font-family: 'Montserrat', sans-serif;
+// font-style: normal;
+// font-weight: 800;
+// font-size: 48px;
+// line-height: 59px;
+// text-align: center;
+// }
+// .col .h2{
+//     margin-bottom: 50px;
+//     font-family: 'Montserrat', sans-serif;
+//     font-style: normal;
+//     font-weight: 800;
+//     font-size: 48px;
+//     line-height: 59px;
+//     text-align: center;
+//     color: #3280B0;
+// }
+// .col .p1{
+//     font-family: 'Montserrat', sans-serif;
+//     width: 600px;
+// font-style: normal;
+// font-weight: 600;
+// font-size: 30px;
+// line-height: 39px;
+// }
+// .first{
+//     display: flex;
+//    align-items: center;
+//    margin-bottom: 50px;
+// }
+
+// .second{
+//     display: flex;
+//    align-items: center;
+//    margin-bottom: 50px;
+// }
+// .third{
+//     display: flex;
+//    align-items: center;
+//    margin-bottom: 50px;
+// }
+// .fouth{
+//     display: flex;
+//    align-items: center;
+//    margin-bottom: 50px;
+// } 
+// .main{
+//     display: flex;
+//     flex-direction: row;
+//     justify-content: center;
+// }
+//  .heading2{
+//     display: flex;
+//    align-items: center;
+//    margin-bottom: 50px;  
+// } 
+//  .heading2 .h3{
+// font-family: 'Montserrat', sans-serif;
+// font-style: normal;
+// font-weight: 800;
+// font-size: 48px;
+// line-height: 59px;
+// text-align: right;
+// color: #3280B0;
+// } 
+// .col p{
+// font-family: 'Montserrat', sans-serif;
+// font-style: normal;
+// font-weight: 600;
+// font-size: 26px;
+// line-height: 46px;
+// }
+// .ikonka{
+//     width: 63px;
+// }
+// @media (max-width: 1200px){
+//     .col .h1{
+//       font-size: 38px;
+//       line-height: 59px;
+//       }
+//       .col .h2{ 
+//           font-size: 38px;
+//           line-height: 49px;
+//       }
+//        .row-cols-2{
+//         display: flex;
+//       flex-direction: column;
+//   } 
+//       .col .p1{
+//    font-size: 28px;
+//       line-height: 35px;
+//       }
+//       .heading2 .h3{
+//         font-size: 38px;
+//         line-height: 49px;
+//         } 
+//       .first{
+//           display: flex;
+//          align-items: center;
+//          margin-bottom: 50px;
+//       }
+      
+    
+//       .second{
+//           display: flex;
+//          align-items: center;
+//          margin-bottom: 50px;
+//       }
+//       .third{
+//           display: flex;
+//          align-items: center;
+//          margin-bottom: 50px;
+//       }
+//       .fouth{
+//           display: flex;
+//          align-items: center;
+//          margin-bottom: 50px;
+//       } 
+//       .main{
+//           display: flex;
+//           flex-direction: column;
+//           justify-content: center;
+      
+//       }
+//       .col p{
+//       font-size: 24px;
+//       line-height:36px;
+//       }
+//       .ikonka{
+//           width: 0px;
+//       }
+//       .img1{
+//           width: 400px;
+//       }  
+//       .chicago{
+//         width: 300px;
+//       }
+//      }
+// @media (max-width: 900px){
+//   .col .h1{
+//     font-size: 38px;
+//     line-height: 59px;
+//     }
+//     .col .h2{ 
+//         font-size: 38px;
+//         line-height: 49px;
+//     }
+//     .col .p1{
+//  font-size: 28px;
+//     line-height: 35px;
+//     }
+//     .heading2 .h3{
+//         font-size: 35px;
+//         line-height: 45px;
+//         }
+//     .col p{
+//     font-size: 24px;
+//     line-height:36px;
+//     }
+//     .ikonka{
+//         width: 0px;
+//     }
+//     .img1{
+//         width: 350px;
+//     }  
+//     .chicago{
+//       width: 250px;
+//     }
+
+//     .row-cols-2{
+//         display: flex;
+//       flex-direction: column;
+//   }
+//    }
+
+// @media (max-width: 800px){
+//     .col .h1{
+//       font-size: 38px;
+//       line-height: 59px;
+//       }
+//       .col .h2{ 
+//           font-size: 38px;
+//           line-height: 49px;
+//       }
+//       .col .p1{
+//    font-size: 28px;
+//       line-height: 35px;
+     
+//       }
+//       .heading2 .h3{
+//           font-size: 35px;
+//           line-height: 45px;
+//           }
+//       .col p{
+//       font-size: 24px;
+//       line-height:36px;
+//       } 
+//       /* .img1{
+//           width: 300px;
+//       }  
+//       .chicago{
+//         width: 200px;
+//       } */
+
+
+
+
+
+//       /* .row-cols-2{
+//         display: flex;
+//       flex-direction: column;
+//   } */
+//      }
+
+//      @media (max-width: 700px){
+//           /* .img1{
+//               width: 250px;
+//           }   */
+//           .col .p1{
+//               width: 500px;
+//               font-size: 18px;
+//           }
+//           /* .chicago{
+//             width:150px;
+//           } */
+     
+// @media (max-width: 500px){
+//     .col .h1{
+//       font-size: 26px;
+//       }
+//       .col .h2{ 
+//           font-size: 28px;
+//           line-height: 39px;
+//       }
+//       .heading2 .h3{
+//           font-size: 16px;
+//           line-height: 25px;
+//           } 
+//       .col p{
+//       font-size: 14px;
+//       line-height:26px;
+//       }
+//       /* .img1{
+//           width: 0px;
+//       }  
+//       .chicago{
+//         width:0px;
+//       }    */
+//      }
+
+//      @media (max-width: 495px){   
+//           .img1{
+//               width: 0px;
+//           }  
+//           .chicago{
+//         width:0px;
+//           }
+
+//             .col .p1{
+//          font-size: 18px;
+//             line-height: 25px;
+//             width: 400px;  
+//             }
+//            .row-cols-2{
+//             width: 10px;
+//         }
+//          }   
+//          @media (max-width: 400px)
+//          {
+//             .col .h1{
+//               font-size: 18px;
+//               line-height: 39px;
+//               }
+//               .col .h2{ 
+//                   font-size: 18px;
+//                   line-height: 29px;
+//               }
+//               .col .p1{
+//            font-size: 16px;
+//               line-height: 20px;
+//               width: 400px;
+//               }
+//               .heading2 .h3{
+//                   font-size: 14px;
+//                   line-height: 20px;
+//                   } 
+//               .first{
+//                   display: flex;
+//                  align-items:start;
+//                  margin-bottom: 40px;
+//               }
+//               .col p{
+//               font-size: 14px;
+//               line-height:18px;
+//               }
+//               .img1{
+//                   width: 0px;
+//               }  
+//               .chicago{
+//                 width:0px;
+//               }
+//                .row-cols-2{
+//                   width: 10px;
+//               }
+//             }}
+
+
+
+//             <!DOCTYPE html>
+// <html lang="en">
+
+// <head>
+//   <meta charset="UTF-8">
+//   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+//   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//   <title>Document</title>
+//   <link rel="stylesheet" href="css/madina.css">
+//   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet"
+//     integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+// </head>
+
+// <body>
+
+//   <section class="main">
+//     <div class="container">
+//       <div class="row row-cols-2">
+//         <div class="col">
+//           <h1 class="h1 h1-responsive">Отправляемый город:</h1>
+//           <h2 class="h2">Chicago</h2>
+//           <p class="p1">город известен как<br> образовательный sentr кроме <br>университетов и колледжей здесь
+//             <br>находится множество семинарии <br>теологический и языковый школ
+//           </p>
+//         </div>
+//         <div class="col">
+//           <img class="img1" src="../img/statuya1.png" width="500px" alt="s">
+//         </div>
+//         <div class="col">
+//           <img class="chicago" src="../img/chikago.png" width="500px" alt="statuya">
+//         </div>
+
+//         <div class="col">
+//           <div class="heading2">
+//             <h3 class="h3">Учебное заведение</h3>
+//           </div>
+
+//           <div class="first">
+//             <img class="ikonka" src="../img/Vector.svg" alt="statuya">
+//             <p class="firstP">University of Chicago(
+//               Чикагский Университет Университет чикаго)</p>
+//           </div>
+
+
+//           <div class="second">
+//             <img class="ikonka" src="../img/Vector.svg" alt="statuya">
+//             <p> illinois institut of Tehnology (IT)
+//               Иллинойсский технологический институт</p>
+//           </div>
+
+//           <div class="third">
+//             <img class="ikonka" src="../img/Vector.svg" alt="statuya">
+//             <p>Loyola University Chicago (LUC) Чикагский Университет Лойолы</p>
+//           </div>
+
+//           <div class="fouth">
+//             <img class="ikonka" src="../img/Vector.svg" alt="statuya">
+//             <p>Concordia university of Chicago Университет Concordia university</p>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+
+//   </section>
+//   <script src="./script.js"></script>
+//   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
+//     integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4"
+//     crossorigin="anonymous"></script>
+// </body>
+
+// </html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@800&display=swap'); 
+// @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@600&display=swap');
+
+// html,body {overflow-x:hidden;
+// }
+
+
+// /*//////////////*/
+// .main{
+//     margin-left: 137px;
+//     display: flex;
+//     flex-direction: row;
+// }
+// /*/////////////////////*/
+// .col .h1{
+//  font-family: 'Montserrat', sans-serif;
+// font-style: normal;
+// font-weight: 800;
+// font-size: 48px;
+// line-height: 59px;
+// text-align: center;
+// }
+// .col .h2{
+//     margin-bottom: 50px;
+//     font-family: 'Montserrat', sans-serif;
+//     font-style: normal;
+//     font-weight: 800;
+//     font-size: 48px;
+//     line-height: 59px;
+//     text-align: center;
+//     color: #3280B0;
+// }
+// .col .p1{
+//     border-left: 5px solid #016096;
+//     /* margin: 5px;*/
+//     padding: 5px; 
+  
+//     font-family: 'Montserrat', sans-serif;
+//     width: 600px;
+// font-style: normal;
+// font-weight: 600;
+// font-size: 30px;
+// line-height: 39px;
+// color: black;
+// }
+
+// .first{
+//     display: flex;
+//    align-items: center;
+//    margin-bottom: 50px;
+//    /* width: 700px;  */
+// }
+// .second{
+//     display: flex;
+//    align-items: center;
+//    margin-bottom: 50px;
+// }
+// .third{
+//     display: flex;
+//    align-items: center;
+//    margin-bottom: 50px;
+// }
+// .fouth{
+//     display: flex;
+//    align-items: center;
+//    margin-bottom: 50px;
+// } 
+// .main{
+//     display: flex;
+//     flex-direction: row;
+//     justify-content: center;
+// }
+//  .heading2{
+//     display: flex;
+//    align-items: center;
+//    margin-bottom: 50px;  
+// } 
+//  .heading2 .h3{
+// font-family: 'Montserrat', sans-serif;
+// font-style: normal;
+// font-weight: 800;
+// font-size: 48px;
+// line-height: 59px;
+// text-align: right;
+// color: #3280B0;
+// } 
+// .col p{
+// font-family: 'Montserrat', sans-serif;
+// font-style: normal;
+// font-weight: 600;
+// font-size: 26px;
+// line-height: 46px;
+// color: black;
+// }
+// .ikonka{
+//     width: 63px;
+// }
+// @media (max-width: 1200px){
+//     .col .h1{
+//       font-size: 38px;
+//       }
+//       .col .h2{ 
+//           font-size: 38px;
+//       }
+//        .row-cols-2{
+//         display: flex;
+//       flex-direction: column;
+//   } 
+
+//   body {overflow-x:hidden;}
+//       .col .p1{
+//    font-size: 28px;
+//       }
+//       .heading2 .h3{
+//         font-size: 38px;
+//         } 
+//       .first{
+//           display: flex;
+//          align-items: start;
+//          margin-bottom: 50px;
+//          width: 400px; 
+//       }
+//       .second{
+//           display: flex;
+//          align-items: center;
+//          margin-bottom: 50px;
+//          width: 400px; 
+//       }
+//       .third{
+//           display: flex;
+//          align-items: center;
+//          margin-bottom: 50px;
+//          width: 400px; 
+//       }
+//       .fouth{
+//           display: flex;
+//          align-items: center;
+//          margin-bottom: 50px;
+//          width: 400px; 
+//       } 
+//       .main{
+//           display: flex;
+//           flex-direction: column;
+//           justify-content: center;
+      
+//       }
+//       .col p{
+//       font-size: 24px;
+//       line-height:36px;
+//       }
+//       .ikonka{
+//           width: 60px;
+//       }
+
+//       /* ////////////////////////////////////////// */
+//       .img1{
+//           width: 400px;
+//       }  
+//       .chicago{
+//         width: 400px;
+//       }
+//       /* //////////////////////////////////////////// */
+//      }
+// @media (max-width: 900px){
+//     .main{
+//         /* ///////////// */
+//         margin-left: 0;
+//     }
+//     /* /////////// */
+//   .col .h1{
+//     font-size: 38px;
+//     }
+//     .col .h2{ 
+//         font-size: 38px;
+//     }
+//     .heading2 .h3{
+//         font-size: 35px;
+//         }
+//     .col p{
+//     font-size: 24px;
+//     }
+//     .ikonka{
+//         width: 0px;
+//     }
+  
+//     .row-cols-2{
+//         display: flex;
+//       flex-direction: column;
+//   }
+//    }
+
+// @media (max-width: 800px){
+//     .col .h1{
+//     width: 300px;
+//       font-size: 38px;
+//       }
+//       .col .h2{ 
+//           font-size: 38px;
+//           width: 300px;
+//       }
+     
+//       .heading2 .h3{
+//           font-size: 35px;
+//           line-height: 45px;
+//           width:300px ;
+//           }
+//       .col p{
+//       font-size: 24px;
+//       line-height:36px;
+//       } 
+//      }
+
+//      @media (max-width: 700px){
+//           .col .p1{
+//               width: 500px;
+//               font-size: 24px;
+//           }
+//         }
+
+// @media (max-width: 500px){
+//     .col .h1{
+//       font-size: 26px;
+//       }
+     
+//       .col .h2{ 
+//           font-size: 28px;
+//           line-height: 39px;
+//       }
+//       .heading2 .h3{
+//           font-size: 16px;
+//           line-height: 25px;
+//           } 
+//       .col p{
+//       font-size: 14px;
+//       line-height:26px;
+//       }
+//      }
+
+//      @media (max-width: 495px){   
+//             .col .p1{
+//          font-size: 18px;
+//             line-height: 25px;
+//             width: 300px;  
+//             }
+//            .row-cols-2{
+//             width: 10px;
+//         }
+//         .col .h1{
+//             font-size: 26px;
+//             }    
+//             .col .h2{ 
+//                 font-size: 28px;
+//                 line-height: 39px;
+//             }
+//             .img1 {
+//                 width: 250px; 
+//                }
+//                .chicago{
+//                    width: 250px;
+//                }
+//             .heading2 .h3{
+//                 font-size: 16px;
+//                 line-height: 25px;
+//                 } 
+//             .col p{
+//             font-size: 14px;
+//             line-height:26px;
+//             }
+//         .first{
+//             display: flex;
+//            align-items: center;
+//            margin-bottom: 50px;
+//            width: 300px; 
+//         }
+//         .second{
+//             display: flex;
+//            align-items: center;
+//            margin-bottom: 50px;
+//            width: 300px; 
+//         }
+//         .third{
+//             display: flex;
+//            align-items: center;
+//            margin-bottom: 50px;
+//            width: 300px; 
+//         }
+//         .fouth{
+//             display: flex;
+//            align-items: center;
+//            margin-bottom: 50px;
+//            width: 300px; 
+//         } 
+//          }   
+//          @media (max-width: 400px)    {
+//             .col .h1{
+//               font-size: 18px;
+//               line-height: 39px;
+//               }
+//               .col .h2{ 
+//                   font-size: 18px;
+//                   line-height: 29px;
+//               }
+//               .col .p1{
+//            font-size: 16px;
+//               line-height: 20px;
+//               width: 400px;
+//               }
+//               .heading2 .h3{
+//                   font-size: 14px;
+//                   line-height: 20px;
+//                   } 
+//               .first{
+//                   display: flex;
+//                  align-items:start;
+//                  margin-bottom: 40px;
+//               }
+//               .col p{
+//               font-size: 14px;
+//               line-height:18px;
+//               }
+//               .img1 {
+//                width: 250px; 
+//               }
+//               .chicago{
+//                   width: 250px;
+//               }
+//                .row-cols-2{
+//                   width: 10px;
+//               }
+//             }
+
+//             @media (min-width: 400px)
+//             {
+//                 .body{
+//                     overflow: hidden;
+//                 }
+//             }
+
+
+
+
+
+            
+
+//  <section class="main">
+//   <div class="containerr">
+//     <div class="row row-cols-2">
+//       <div class="col">
+//         <h1 class="h1 h1-responsive">Отправляемый город:</h1>
+//         <h2 class="h2">Chicago</h2>
+//         <p class="p1">город известен как<br> образовательный sentr кроме <br>университетов и колледжей здесь
+//           <br>находится множество семинарии <br>теологический и языковый школ
+//         </p>
+//       </div>
+//       <div class="col">
+//         <img class="img1" src="../img/chicago10.png" width="500px" alt="s">
+//       </div>
+//       <div class="col">
+//         <img class="chicago" src="../img/statuya10.png" width="500px" alt="statuya">
+//       </div>
+
+//       <div class="col">
+//         <div class="heading2">
+//           <h3 class="h3">Учебное заведение</h3>
+//         </div>
+
+//         <div class="first">
+//           <img class="ikonka" src="../img/Vector.svg" alt="statuya">
+//           <p class="firstP">University of Chicago(
+//             Чикагский Университет Университет чикаго)</p>
+//         </div>
+
+
+//         <div class="second">
+//           <img class="ikonka" src="../img/Vector.svg" alt="statuya">
+//           <p> illinois institut of Tehnology (IT)
+//             Иллинойсский технологический институт</p>
+//         </div>
+
+//         <div class="third">
+//           <img class="ikonka" src="../img/Vector.svg" alt="statuya">
+//           <p>Loyola University Chicago (LUC) Чикагский Университет Лойолы</p>
+//         </div>
+
+//         <div class="fouth">
+//           <img class="ikonka" src="../img/Vector.svg" alt="statuya">
+//           <p>Concordia university of Chicago Университет Concordia university</p>
+//         </div>
+//       </div>
+//     </div>
+//   </div>
+
+// </section>
+
+//   <!--FOOTER-->
+//   <!--FOOTER-->
+//   <!--FOOTER-->

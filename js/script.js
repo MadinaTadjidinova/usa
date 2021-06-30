97d33625661326e233d37c655581b0baaa0b404a
MadinaTadjidinova <madina.tadjidinova@gmail.com>
2021-06-30 12:17:50 +0600

    do commit js values

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// /////////let navHide = document.getElementsByClassName('navbar');
+// let navFix = document.getElementsByClassName('container');
+/////////     if(pos > 10){
+//         navFix[0].style.top = "0px";
+//         navHide[0].style.top = "-50px";
+//         navFix[0].style.position = "fixed";
+///////////         navHide[0].style.position = "fixed";
+//         navFix[0].style.zIndex = "1";
+
+//     }else if(pos < 10){
+//         navFix[0].style.top = "50px";
+//         navHide[0].style.top = "0px";
+//         navFix[0].style.position = "static";
+//         navHide[0].style.position = "static";
+//     }
+// };
